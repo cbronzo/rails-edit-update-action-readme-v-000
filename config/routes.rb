6d681bfa10cda8a4858bbb53d3525e2664bf1996
Catherine Bronzo <cbronzo@mac.com>
2018-09-19 02:56:35 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   
   get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
   patch 'posts/:id', to: 'posts#update'
+  
+  # or
+  
+  #   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
